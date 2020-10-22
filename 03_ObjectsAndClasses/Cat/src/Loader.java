bde32d3e12534701c7c3cc07887657c7caa2ae24
александр <vipermad91@gmail.com>
2020-10-22 20:41:07 +0300

    class and object - cat experiments

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -1,10 +1,41 @@
+import org.w3c.dom.ls.LSOutput;
 
 public class Loader
 {
     public static void main(String[] args)
     {
         Cat cat = new Cat();
+        Cat barsik = new Cat();
+        Cat moris = new Cat();
+        Cat kitiket = new Cat();
+        Cat tiger = new Cat();
+        Cat richard = new Cat();
+        Cat leo = new Cat();
 
-        System.out.println(cat.getStatus());
+        //кормим кошечек
+        cat.feed(1000.0);
+        barsik.feed(500.0);
+        tiger.feed(1500.0);
+
+        //получаем их вес.
+        System.out.println("Weight cat: " + cat.getWeight() + " g.");
+        System.out.println("Weight barsik: " + barsik.getWeight() + " g.");
+        System.out.println("Weight tiger: " + tiger.getWeight() + " g.");
+
+        //перекармливаем кошку до взрыва.
+        while(cat.getStatus() != "Exploded")
+        {
+            cat.feed(1000.0);
+            }
+        System.out.println("Cat status: "+ cat.getStatus());
+
+        //Замяукаем кошку
+        while (barsik.getStatus() != "Dead")
+        {
+            barsik.meow();
+            }
+        System.out.println("Barsik status: " + barsik.getStatus());
+
+        }
+
     }
-}
