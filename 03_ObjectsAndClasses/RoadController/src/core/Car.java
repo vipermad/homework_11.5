85f27d20685e0f4d968dd2df806bd2784f4a90b9
александр <vipermad91@gmail.com>
2020-10-23 22:10:28 +0300

    incapsulation, getters and setters

--- 03_ObjectsAndClasses/RoadController/src/core/Car.java
+++ 03_ObjectsAndClasses/RoadController/src/core/Car.java
@@ -1,18 +1,56 @@
 package core;
 
-public class Car
-{
+public class Car {
     public String number;
     public int height;
     public double weight;
     public boolean hasVehicle;
     public boolean isSpecial;
 
-    public String toString()
-    {
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setHasVehicle(boolean hasVehicle) {
+        this.hasVehicle = hasVehicle;
+    }
+
+    public boolean getHasVehicle() {
+        return hasVehicle;
+    }
+
+    public boolean isSpecial() {
+        return isSpecial;
+    }
+
+    public void setSpecial(boolean special) {
+        isSpecial = special;
+    }
+
+    public String toString() {
         String special = isSpecial ? "СПЕЦТРАНСПОРТ " : "";
         return "\n=========================================\n" +
-            special + "Автомобиль с номером " + number +
-            ":\n\tВысота: " + height + " мм\n\tМасса: " + weight + " кг";
+                special + "Автомобиль с номером " + number +
+                ":\n\tВысота: " + height + " мм\n\tМасса: " + weight + " кг";
     }
 }
