326a3e377f3b4d99c94718ae19db5a2386aec6e0
unknown <vipermad91@gmail.com>
2020-10-27 23:34:03 +0300

    external library connection - airport.jar

--- 03_ObjectsAndClasses/Airports/src/Main.java
+++ 03_ObjectsAndClasses/Airports/src/Main.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import com.skillbox.airport.Aircraft;
+import com.skillbox.airport.Airport;
 
 public class Main {
+    public static void main(String[] args) {
+        Airport airport = Airport.getInstance();
+        int numberOfAircraft = airport.getAllAircrafts().size();
+        airport.getAllAircrafts().forEach(System.out::println);
+        System.out.println("Всего самолетов: " + numberOfAircraft);
+    }
 }
