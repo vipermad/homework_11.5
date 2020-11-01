ebeba1a3550c72ee3f1e7fb50e54a02c81a45595
vipermad91@gmail.com <fkg7h4b3v6>
2020-11-01 20:46:51 +0300

    home work 4.3 part 1 done.

--- 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/GeometryCalculator.java
+++ 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/GeometryCalculator.java
@@ -1,21 +1,36 @@
+import java.rmi.MarshalException;
+
 public class GeometryCalculator {
-    // метод должен использовать абсолютное значение radius
-    public static double getCircleSquare(double radius) {
-        return 0;
-    }
 
-    // метод должен использовать абсолютное значение radius
-    public static double getSphereVolume(double radius) {
-        return 0;
-    }
+  // метод должен использовать абсолютное значение radius
+  public static double getCircleSquare(double radius) {
 
-    public static boolean isTrianglePossible(double a, double b, double c) {
-        return false;
-    }
+    return Math.abs(Math.PI * Math.pow(radius, 2));
+  }
 
-    // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
-    // методом isTrianglePossible, если невозможен вернуть -1.0
-    public static double getTriangleSquare(double a, double b, double c) {
-        return 0;
+  // метод должен использовать абсолютное значение radius
+  public static double getSphereVolume(double radius) {
+
+    return Math.abs(4. / 3. * Math.PI * Math.pow(radius, 3));
+  }
+
+  public static boolean isTrianglePossible(double a, double b, double c) {
+    if (a + b > c && a + c > b && c + b > a) {
+      return true;
+    } else {
+      return false;
     }
+  }
+
+  // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
+  // методом isTrianglePossible, если невозможен вернуть -1.0
+  public static double getTriangleSquare(double a, double b, double c) {
+    if (isTrianglePossible(a, b, c)) {
+      double halfMeter = (a + b + c) / 2.;
+      double S = Math.sqrt(halfMeter*((halfMeter-a)*(halfMeter-b)*(halfMeter-c)));
+      return S;
+    } else {
+      return -1;
+    }
+  }
 }
