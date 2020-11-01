9ccb74beb78fcdee3b79b338b00837c4c5fcd847
vipermad91@gmail.com <fkg7h4b3v6>
2020-11-01 16:31:07 +0300

    Homework 4.2 done

---
+++ 04_NumbersStringsAndDates/homework_4.2/maxAndMinValue/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Max value Double: " + Double.MAX_VALUE);
+    System.out.println("Min value Double: " + Double.MIN_VALUE);
+    System.out.println("Max value Float: " + Float.MAX_VALUE);
+    System.out.println("Min value Float: " + Float.MIN_VALUE);
+    System.out.println("Max value Byte: " + Byte.MAX_VALUE);
+    System.out.println("Min value Byte: " + Byte.MIN_VALUE);
+    System.out.println("Max value Int: " + Integer.MAX_VALUE);
+    System.out.println("Min value Int: " + Integer.MIN_VALUE);
+    System.out.println("Max value Short: " + Short.MAX_VALUE);
+    System.out.println("Min value Short: " + Short.MIN_VALUE);
+    System.out.println("Max value Long: " + Long.MAX_VALUE);
+    System.out.println("Min value Long: " + Long.MIN_VALUE);
+  }
+
+}
