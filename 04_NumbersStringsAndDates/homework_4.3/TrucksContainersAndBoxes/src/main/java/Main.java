04d148ba1f9f7136c9361bc6c552508fcf166be0
vipermad91@gmail.com <fkg7h4b3v6>
2020-11-04 20:54:14 +0300

    home work 4.3 part 2(task with *)  done.

--- 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -2,13 +2,49 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String boxes = scanner.nextLine();
+  public static final double VOLUME_CONTAINER = 27;
+  public static final double VOLUME_TRUCK = 12 * VOLUME_CONTAINER;
 
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
-        // пример вывода при вводе 2
-        // для отступа используйте табуляцию - \t
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int boxes = Integer.parseInt(scanner.nextLine());
+    int needTruck = (int) Math.ceil(boxes / VOLUME_TRUCK);
+    int needContainer = (int) Math.ceil(boxes / VOLUME_CONTAINER);
+
+    int truck = 0;      // 12 container.
+    int container = 0;  // 27 box
+    int countBox = 0;        // счетчик коробок
+    boolean flagFirstStart = true;
+    if (boxes > 0) {
+      truck++;
+      container++;
+      countBox++;
+    }
+    for (int i = 1; i != boxes+1; i++) {
+      if (flagFirstStart){
+        System.out.println("Грузовик: " + truck);
+        truck++;
+        System.out.println("\tКонтейнер: " + container);
+        container++;
+        flagFirstStart = false;
+      }
+      if ((i-1) % VOLUME_TRUCK == 0 && i != 1) {
+        System.out.println("Грузовик: " + truck);
+        truck++;
+      }
+      if ((i-1) % VOLUME_CONTAINER == 0 && i != 1) {
+        System.out.println("\tКонтейнер: " + container);
+        container++;
+      }
+      System.out.println("\t\tЯщик: " + countBox);
+      countBox++;
+    }
+
+    System.out.println("Необходимо: \nгрузовиков - " + needTruck + " шт." + "\nконтейнеров - " + needContainer + " шт.");
+
+    // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
+    // пример вывода при вводе 2
+    // для отступа используйте табуляцию - \t
 
         /*
         Грузовик: 1
@@ -19,6 +55,6 @@ public class Main {
         грузовиков - 1 шт.
         контейнеров - 1 шт.
         */
-    }
+  }
 
 }
