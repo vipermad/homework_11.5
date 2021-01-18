715c3c88abaef580b3750ecb2f5eeda4f9d602a4
vipermad <vipermad91@gmail.com>
2021-01-18 14:59:14 +0300

    in progress...

--- 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/Main.java
@@ -1,7 +1,44 @@
-public class Main {
-    private static TodoList todoList = new TodoList();
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public static void main(String[] args) {
-        // TODO: написать консольное приложение для работы со списком дел todoList
+public class Main {
+
+  private static TodoList todoList = new TodoList();
+
+  public static void main(String[] args) {
+    // TODO: написать консольное приложение для работы со списком дел todoList
+
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+      String input = scanner.nextLine();
+      if (input.equals("0")) {
+        System.out.println("EXIT");
+        break;
+      }
+      Pattern patternAdd = Pattern.compile(".*ADD.*", Pattern.CASE_INSENSITIVE);
+      Matcher matcherAdd = patternAdd.matcher(input);
+      Pattern patternEdit = Pattern.compile(".*EDIT.*", Pattern.CASE_INSENSITIVE);
+      Matcher matcherEdit = patternEdit.matcher(input);
+      Pattern patternDelete = Pattern.compile(".*DELETE.*", Pattern.CASE_INSENSITIVE);
+      Matcher matcherDelete = patternDelete.matcher(input);
+      Pattern patternList = Pattern.compile(".*LIST.*", Pattern.CASE_INSENSITIVE);
+      Matcher matcherList = patternList.matcher(input);
+      if (matcherAdd.matches()) {
+        todoList.add(input.substring(4));
+        System.out.println("Мы тут");
+      }
+      if (matcherEdit.matches()) {
+        todoList.edit(input.substring(7),
+            Integer.parseInt(input.substring(5, 6).trim()));
+      }
+      if (matcherDelete.matches()) {
+        todoList.delete(Integer.parseInt(input.substring(6,8).trim()));
+      }
+      if (matcherList.matches()) {
+        todoList.getTodos();
+      }
     }
+  }
 }
--- 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/TodoList.java
+++ 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/TodoList.java
@@ -2,28 +2,60 @@ import java.util.ArrayList;
 
 public class TodoList {
 
-    public void add(String todo) {
-        // TODO: добавьте переданное дело в конец списка
+  private ArrayList<String> arrayList = new ArrayList<>();
+
+  public void add(String todo) {
+    // TODO: добавьте переданное дело в конец списка
+    arrayList.add(todo);
+    System.out.println("Добавлено дело \"" + todo + "\"");
+  }
+
+  public void add(int index, String todo) {
+    // TODO: добавьте дело на указаный индекс,
+    //  проверьте возможность добавления
+    if (index <= (arrayList.size()-1) && index >= 0) {
+      arrayList.add(index, todo);
+      System.out.println("Добавлено дело \"" + todo + "\"");
+    } else if(index > arrayList.size()) {
+      arrayList.add(todo);
+      System.out.println("Добавлено дело \"" + todo + "\"");
     }
 
-    public void add(int index, String todo) {
-        // TODO: добавьте дело на указаный индекс,
-        //  проверьте возможность добавления
+  }
+
+  public void edit(String todo, int index) {
+    // TODO: заменить дело на index переданным todo индекс,
+    //  проверьте возможность изменения
+      if (!(index >arrayList.size())) {
+        System.out.println("Дело \"" + arrayList.get(index) + "\" заменено на " + "\"" + todo + "\"");
+        arrayList.set(index, todo);
+        // TODO: Почему то не срабатывает делет на введенный последний индекс
+      } else {
+        System.out.println("Введенный индекс находится за пределами списка");
+        System.out.println("Введите индекс от 0" + " до " + (arrayList.size() - 1));
+      }
     }
 
-    public void edit(String todo, int index) {
-        // TODO: заменить дело на index переданным todo индекс,
-        //  проверьте возможность изменения
+
+  public void delete(int index) {
+    // TODO: удалить дело находящееся по переданному индексу,
+    //  проверьте возможность удаления дела
+    if (index < (arrayList.size() - 1) && index >= 0) {
+      System.out.println("Дело \"" + arrayList.get(index) + "\" удалено ");
+      arrayList.remove(index);
+    } else {
+      System.out.println("Введенный индекс находится за пределами списка");
+      System.out.println("Введите индекс от 0" + " до " + (arrayList.size() - 1));
     }
 
-    public void delete(int index) {
-        // TODO: удалить дело находящееся по переданному индексу,
-        //  проверьте возможность удаления дела
-    }
+  }
 
-    public ArrayList<String> getTodos() {
-        // TODO: вернуть список дел
-        return new ArrayList<>();
+  public ArrayList<String> getTodos() {
+    // TODO: вернуть список дел
+    for (int i = 0; i < arrayList.size(); i++) {
+      System.out.println(i + " - " + arrayList.get(i));
     }
+    return arrayList;
+  }
 
 }
