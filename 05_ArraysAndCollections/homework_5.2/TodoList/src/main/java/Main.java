000ce5c6f9de9de53c2a4e312dee2f7b977ef1ef
vipermad91@gmail.com <fkg7h4b3v6>
2021-01-26 22:48:03 +0300

    additional task 5.2

--- 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/Main.java
@@ -16,27 +16,26 @@ public class Main {
         System.out.println("EXIT");
         break;
       }
-      Pattern patternAdd = Pattern.compile(".*ADD.*", Pattern.CASE_INSENSITIVE);
-      Matcher matcherAdd = patternAdd.matcher(input);
-      Pattern patternEdit = Pattern.compile(".*EDIT.*", Pattern.CASE_INSENSITIVE);
-      Matcher matcherEdit = patternEdit.matcher(input);
-      Pattern patternDelete = Pattern.compile(".*DELETE.*", Pattern.CASE_INSENSITIVE);
-      Matcher matcherDelete = patternDelete.matcher(input);
-      Pattern patternList = Pattern.compile(".*LIST.*", Pattern.CASE_INSENSITIVE);
-      Matcher matcherList = patternList.matcher(input);
-      if (matcherAdd.matches()) {
-        todoList.add(input.substring(4));
+      Pattern pattern = Pattern
+          .compile("(?<command>ADD|EDIT|DELETE|LIST)(\\s(?<index>\\d+))?(\\s(?<text>.+))?",
+              Pattern.CASE_INSENSITIVE);
+      Matcher matcher = pattern.matcher(input);
+      if (matcher.find()) {
+        if (matcher.group("command").equals("ADD")) {
+          todoList.add(matcher.group("text"));
+        }
+        if (matcher.group("command").equals("EDIT")) {
+          todoList.edit(matcher.group("text"),
+              Integer.parseInt(matcher.group("index").trim()));
+        }
+        if (matcher.group("command").equals("DELETE")) {
+          todoList.delete(Integer.parseInt(matcher.group("index").trim()));
+        }
+        if (matcher.group("command").equals("LIST")) {
+          todoList.getTodos();
+        }
       }
-      if (matcherEdit.matches()) {
-        todoList.edit(input.substring(7),
-            Integer.parseInt(input.substring(5, 6).trim()));
-      }
-      if (matcherDelete.matches()) {
-        todoList.delete(Integer.parseInt(input.substring(6,8).trim()));
-      }
-      if (matcherList.matches()) {
-        todoList.getTodos();
-      }
+
     }
   }
 }
