ebecd33ca9a7a4e0c33d32a92199af851efa6a10
александр <vipermad91@gmail.com>
2021-03-12 18:08:05 +0300

    Task 8.1 Complete

--- 08_ExceptionsDebuggingAndTesting/homework_8.1/ConsoleCustomerList/src/main/java/Customer.java
+++ 08_ExceptionsDebuggingAndTesting/homework_8.1/ConsoleCustomerList/src/main/java/Customer.java
@@ -1,4 +1,5 @@
 public class Customer {
+
     private final String name;
     private final String phone;
     private final String email;
--- 08_ExceptionsDebuggingAndTesting/homework_8.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/homework_8.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,37 +1,74 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomerStorage {
-    private final Map<String, Customer> storage;
+  private final Map<String, Customer> storage;
 
-    public CustomerStorage() {
-        storage = new HashMap<>();
+  public CustomerStorage() {
+    storage = new HashMap<>();
+  }
+
+  public void addCustomer(String data) {
+    final int INDEX_NAME = 0;
+    final int INDEX_SURNAME = 1;
+    final int INDEX_EMAIL = 2;
+    final int INDEX_PHONE = 3;
+
+    String[] components = data.split("\\s+");
+    if (components.length != 4) {
+      throw new ArrayIndexOutOfBoundsException();
+    }
+    String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
+    if (components[INDEX_PHONE].length() != 12) {
+      throw new NumberFormatException();
+    }
+    if (!emailFormat(components[INDEX_EMAIL])){
+      throw new IllegalArgumentException();
+    }
+    storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+  }
+
+  public void listCustomers() {
+    storage.values().forEach(System.out::println);
+  }
+
+  public void removeCustomer(String name) {
+    try {
+      storage.remove(name);
+    }catch (IllegalArgumentException exception){
+      exception.printStackTrace();
     }
 
-    public void addCustomer(String data) {
-        final int INDEX_NAME = 0;
-        final int INDEX_SURNAME = 1;
-        final int INDEX_EMAIL = 2;
-        final int INDEX_PHONE = 3;
+  }
 
-        String[] components = data.split("\\s+");
-        String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
-        storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
-    }
+  public Customer getCustomer(String name) {
+    return storage.get(name);
+  }
 
-    public void listCustomers() {
-        storage.values().forEach(System.out::println);
+  public int getCount() {
+    return storage.size();
+  }
+    //Почему то с этой регулярка не работает:(
+  private boolean phoneFormat(String phoneNumber) {
+    String regexPhoneNumber = "^7\\d{10}$";
+    Pattern patternPhoneNumber = Pattern.compile(regexPhoneNumber);
+    Matcher matcherPhone = patternPhoneNumber.matcher(phoneNumber);
+    if (matcherPhone.find()) {
+      return true;
     }
+    return false;
+  }
 
-    public void removeCustomer(String name) {
-        storage.remove(name);
+  private boolean emailFormat(String email) {
+    // TODO: валидный формат email добавляется
+    Pattern VALID_EMAIL_ADDRESS_REGEX =
+        Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+    Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+    if (matcher.find()) {
+      return true;
     }
-
-    public Customer getCustomer(String name) {
-        return storage.get(name);
-    }
-
-    public int getCount() {
-        return storage.size();
-    }
+    return false;
+  }
 }
