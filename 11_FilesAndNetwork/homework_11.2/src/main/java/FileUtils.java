c879489d8fcc95f4432b8f5fe49cdc167ae3eacb
vipermad91@gmail.com <fkg7h4b3v6>
2021-04-06 20:26:50 +0300

    Task 11.2 Complete

--- 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
@@ -1,5 +1,11 @@
+import java.io.File;
+import java.io.IOException;
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
-    }
+
+  public static void copyFolder(String sourceDirectory, String destinationDirectory)
+      throws IOException {
+    // TODO: write code copy content of sourceDirectory to destinationDirectory    File folder = new File(sourceDirName);
+    org.apache.commons.io.FileUtils.copyDirectory(new File(sourceDirectory), new File(destinationDirectory));
+  }
 }
--- 11_FilesAndNetwork/homework_11.2/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/Main.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+
+  public static void main(String[] args) {
+
+    try (Scanner scanner = new Scanner(System.in)) {
+      System.out.println("Введите путь до копируевой папки:");
+      String sourceInput =  scanner.nextLine();
+      System.out.println("Введите путь куда копировать:");
+      String destInput = scanner.nextLine();
+      FileUtils.copyFolder(sourceInput,destInput);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 }
