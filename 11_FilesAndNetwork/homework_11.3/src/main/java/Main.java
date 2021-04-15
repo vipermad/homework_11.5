7058210d21bcc554ae39416916217f9874a07511
vipermad91@gmail.com <fkg7h4b3v6>
2021-04-15 09:57:07 +0300

    intermediate commit

--- 11_FilesAndNetwork/homework_11.3/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.3/src/main/java/Main.java
@@ -1,4 +1,25 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-    }
+
+  private static final String MOVEMENTS_FILE = "src/test/resources/movementList.csv";
+
+  public static void main(String[] args) throws IOException {
+    String line = Files.readString(Paths.get(MOVEMENTS_FILE));
+    System.out.println(line);
+//    //List<Movements> extract = Movements.loadMovementsFromFile(MOVEMENTS_FILE);
+//    System.out.println(extract.isEmpty());
+//    System.out.println(extract.get(1).getOperationDescription());
+//    for (Movements movements: extract
+//    ) {
+//      System.out.println(movements.getAccountNumber());
+//      System.out.println(movements.getCurrency());
+//      System.out.println(movements.getExpense());
+//      System.out.println(movements.getOperationDescription());
+//    }
+  }
 }
--- 11_FilesAndNetwork/homework_11.3/src/main/java/Movements.java
+++ 11_FilesAndNetwork/homework_11.3/src/main/java/Movements.java
@@ -1,13 +1,86 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
 public class Movements {
 
-    public Movements(String pathMovementsCsv) {
+  private String accountType;
+  private String accountNumber;
+  private String currency;
+  private String dateOfOperation;
+  private String transactionReference;
+  private String operationDescription;
+  private Integer incoming;
+  private Integer expense;
+
+  public Movements(String accountType,
+      String accountNumber,
+      String currency,
+      String dateOfOperation,
+      String transactionReference,
+      String operationDescription,
+      Integer incoming,
+      Integer expense) {
+    this.accountType = accountType;
+    this.accountNumber = accountNumber;
+    this.currency = currency;
+    this.dateOfOperation = dateOfOperation;
+    this.transactionReference = transactionReference;
+    this.operationDescription = operationDescription;
+    this.incoming = incoming;
+    this.expense = expense;
+  }
+  public Movements(String path){
+    loadMovementsFromFile(path);
+  }
+
+    public static List<Movements> loadMovementsFromFile(String path) {
+        List<Movements> movements = new ArrayList<>();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(path));
+
+            for (String line : lines) {
+                String[] fragments = line.split(",");
+                if (fragments.length != 8) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+                String dateFormat = "dd.MM.yy";
+              System.out.println(fragments[6]);
+              System.out.println(fragments[6].length());
+                movements.add(new Movements(
+                    fragments[0],
+                    fragments[1],
+                    fragments[2],
+                    fragments[3],
+                    fragments[4],
+                    fragments[5],
+                    Integer.parseInt(fragments[6]),
+                    Integer.parseInt(fragments[7])
+                ));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return movements;
     }
 
-    public double getExpenseSum() {
-        return 0.0;
-    }
+  public double getExpenseSum() {
 
-    public double getIncomeSum() {
-        return 0.0;
-    }
+      return 0.0;
+  }
+
+  public double getIncomeSum() {
+
+      return 0.0;
+  }
 }
